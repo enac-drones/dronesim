2e24b1b71d752a2220f45c2ce4e4d228a4d1bb51
Murat Bronz <muratbronz@gmail.com>
2023-03-28 17:05:05 +0200

    added scipy to the setup required packages

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 
 setup(packages=['dronesim'],
     version='0.1.0',
-    install_requires=['numpy', 'Pillow', 'matplotlib', 'cycler', 'gym', 'pybullet']
+    install_requires=['numpy', 'scipy', 'Pillow', 'matplotlib', 'cycler', 'gym', 'pybullet']
 )
